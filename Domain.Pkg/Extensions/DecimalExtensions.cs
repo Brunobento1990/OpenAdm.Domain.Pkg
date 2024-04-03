d31ac3716e121beffa38cb570679aa4dfddcc5d5
Bruno <brunobentocaina@gmail.com>
2024-04-02 23:55:59 -0300

    fix: decimal format money

--- Domain.Pkg/Extensions/DecimalExtensions.cs
+++ Domain.Pkg/Extensions/DecimalExtensions.cs
@@ -4,7 +4,8 @@ public static class DecimalExtensions
 {
     public static string FormatMoney(this decimal value)
     {
-        var newValue = value.ToString();
-        return newValue.Replace(".", ",")[..(newValue.Length - 2)];
+        var newValue = value.ToString().Split(".");
+        var decimals = newValue[1].Length > 2 ? newValue[1][..2] : newValue[1];
+        return $"{newValue[0]},{decimals}";
     }
 }
