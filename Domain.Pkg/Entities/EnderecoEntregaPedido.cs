e30c50795457fdea7ceffc5363073e7cb99e6ef7
Bruno <brunobentocaina@gmail.com>
2024-07-06 10:28:38 -0300

    add entrega pedido

---
+++ Domain.Pkg/Entities/EnderecoEntregaPedido.cs
@@ -0,0 +1,37 @@
+﻿using Domain.Pkg.Entities.Bases;
+
+namespace Domain.Pkg.Entities;
+
+public sealed class EnderecoEntregaPedido : BaseEntity
+{
+    public EnderecoEntregaPedido(
+        Guid id,
+        DateTime dataDeCriacao,
+        DateTime dataDeAtualizacao,
+        long numero,
+        string cep,
+        decimal frete,
+        string logradouro,
+        string? complemento,
+        string bairro,
+        string localidade,
+        string uf)
+            : base(id, dataDeCriacao, dataDeAtualizacao, numero)
+    {
+        Cep = cep;
+        Frete = frete;
+        Logradouro = logradouro;
+        Complemento = complemento;
+        Bairro = bairro;
+        Localidade = localidade;
+        Uf = uf;
+    }
+
+    public string Cep { get; private set; }
+    public decimal Frete { get; private set; }
+    public string Logradouro { get; private set; }
+    public string? Complemento { get; private set; }
+    public string Bairro { get; private set; }
+    public string Localidade { get; private set; }
+    public string Uf { get; private set; }
+}
